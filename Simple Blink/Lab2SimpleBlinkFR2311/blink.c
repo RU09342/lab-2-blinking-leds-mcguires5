f9f15dd4ba5e535f8f3f8feeea78f682b9510352
SEAN <mcguires5@students.rowan.edu>
2017-09-16 15:36:03 -0400

    Pushing Agagin

--- Simple Blink/Lab2SimpleBlinkFR2311/blink.c
+++ Simple Blink/Lab2SimpleBlinkFR2311/blink.c
@@ -22,12 +22,12 @@ void main(void) {
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
-    P1DIR |= (PIN0);                          // Set P1.0 to output direction
+    P1DIR |= (0x01);                          // Set P1.0 to output direction
 
     for(;;) {
         volatile unsigned int i;            // volatile to prevent optimization
 
-        P1OUT ^= (PIN0) ;                      // Toggle P1.0 using exclusive-OR
+        P1OUT ^= (0x01) ;                      // Toggle P1.0 using exclusive-OR
 
         i = 10000;                          // SW Delay
         do i--;
--- Simple Blink/lab2SimpleBlinkF5529/blink.c
+++ Simple Blink/lab2SimpleBlinkF5529/blink.c
@@ -7,11 +7,13 @@
 void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= (PIN0);					// configure P1.0 as output
+	P1DIR |= 0x01;					// configure P1.0 as output
+
+	volatile unsigned int i;		// volatile to prevent optimization
 
 	while(1)
 	{
-		P1OUT ^= (PIN0);				// toggle P1.0
-		__delay_cycles(100000);     // delay
+		P1OUT ^= 0x01;				// toggle P1.0
+		for(i=10000; i>0; i--);     // delay
 	}
 }
